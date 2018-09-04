cc66e6a22f72391805164d712c1eaa6198a6d7e5
yjl <yjl@yjldeAir.lan>
2018-09-05 00:09:23 +0800

    restfulHttpSend

--- NetTest/ViewController.m
+++ NetTest/ViewController.m
@@ -14,6 +14,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic,strong) NSURLSession* curSession;
+
 @property (nonatomic,strong) NSString* accessToken;
 
 @end
@@ -22,48 +24,74 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self initSession];
+    [self initView];
+    
     // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+#pragma mark initData
+//初始化一个NSUrlSession对象，之后的报文发送都使用该对象
+- (void)initSession
+{
+    //初始化一个session的配置对象
+    NSURLSessionConfiguration* yConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    /*初始化一个session对象，其中三个参数分别是
+     *configuration:配置对象
+     *delegate:session处理代理的对象
+     *delegateQueue：代理的消息处理的线程，这里传mainQueue，代理的消息都会在主线程中收到
+    */
+    self.curSession = [NSURLSession sessionWithConfiguration:yConfiguration delegate:self delegateQueue:[NSOperationQueue mainQueue]];
+}
+
+//初始化界面按钮显示
+- (void)initView
+{
+    int curY = 50;
+    int curX = 30;
+    
+    UIButton* customRestfulBut = [[UIButton alloc] initWithFrame:CGRectMake(curX, curY, self.view.frame.size.width - 2*curX, 30)];
+    customRestfulBut.backgroundColor = [UIColor blueColor];
+    [customRestfulBut setTitle:@"普通报文发送" forState:UIControlStateNormal];
+    [customRestfulBut addTarget:self action:@selector(customRestfulTest) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:customRestfulBut];
+    curY = curY + 50;
+    
+}
+
+-(void)customRestfulTest
+{
+    
+//    //初始化一个session对象
+//    NSURLSession* testSession = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    
+    //初始化一个request对象
     NSURL* curUrl = [NSURL URLWithString:@"https://open.beva.com/v1/auth/get-salt-code"];
     NSMutableURLRequest* curRequest = [[NSMutableURLRequest alloc] initWithURL:curUrl];
     
-    NSURLSessionConfiguration* curConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
-    NSURLSession* curSession = [NSURLSession sessionWithConfiguration:curConfiguration];
-    
-    NSURLSessionDataTask* curTask = [curSession dataTaskWithRequest:curRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+    //生成一个task对象
+    NSURLSessionDataTask* curTask = [self.curSession dataTaskWithRequest:curRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         NSString* saltString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-        NSDictionary* saltDic = [self dictionaryWithJsonString:saltString];
-        NSString* curSalt = [saltDic objectForKey:@"data"];
-        NSLog(@"%@",[NSThread currentThread]);
-        [self performSelectorOnMainThread:@selector(showBackView) withObject:nil waitUntilDone:NO];
+        NSDictionary* dataDic = [self dictionaryWithJsonString:saltString];
+        NSLog(@"receive data : %@,in %@",[dataDic description],[NSThread currentThread]);
         
-        
-        
-        NSString* curSecret = [NSString stringWithFormat:@"%@%@",bava_appSecret,curSalt];
-        
-        
-        
-        NSString* atUrlString = [[NSString alloc] initWithFormat:@"https://open.beva.com/v1/auth/get-access-token1?appkey=%@&salt=%@&secret=%@&devId=123&lang=cn&device=iphone8",beva_appKey,curSalt,[self getmd5WithString:curSecret]];
-        
-        NSURL* curUrl = [NSURL URLWithString:atUrlString];
-        NSMutableURLRequest* curRequest = [[NSMutableURLRequest alloc] initWithURL:curUrl];
-        
-        NSURLSessionConfiguration* curConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
-        NSURLSession* curSession = [NSURLSession sessionWithConfiguration:curConfiguration];
-        
-        NSURLSessionDataTask* atTask = [curSession dataTaskWithRequest:curRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-            NSString* atDataString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-            
-            
-            NSLog(@"%@",[NSThread currentThread]);
-            [self performSelectorOnMainThread:@selector(showBackView) withObject:nil waitUntilDone:NO];
-            
-        }];
-        [atTask resume];
+//        //返回主线程刷新界面
+//        [self performSelectorOnMainThread:@selector(refreshView) withObject:nil waitUntilDone:NO];
+//        
+//        //返回主线程刷新界面
+//        dispatch_async(dispatch_get_main_queue(), ^{
+//            //刷新页面
+//        });
         
     }];
     
+    //启动task任务
     [curTask resume];
 }
+
 
 - (NSString*)getmd5WithString:(NSString *)string
 
@@ -105,11 +133,7 @@
     return dic;
 }
 
-- (void)showBackView
-{
-    NSLog(@"%@",[NSThread currentThread]);
-    self.view.backgroundColor = [UIColor redColor];
-}
+
 
 
 - (void)didReceiveMemoryWarning {
@@ -117,5 +141,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark NSURLSessionDelegate
 
 @end
